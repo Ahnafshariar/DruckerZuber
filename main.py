cd398d972321377c4a91912db6a38461f63e3df3
Ahnaf Shariar <ahnafshariar482@gmail.com>
2019-06-13 02:26:06 +0600

    update

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ start = time.time()
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
 
+import csv
+
 # Set up the base URL
 url = "https://www.druckerzubehoer.de/shop/subcategory/vcatid/DV-TinteToner/catid/D-Brother/lng/de_DE/site/1/lng/de_DE"
 
@@ -41,8 +43,8 @@ for (k, v) in json_data.items():
     name_list = value_json.strip('[]')
     # print(name_list)
 
-
-split1 = name_list.split("', 'selected': False, 'topseller_groups': [], 'topseller_subcategories': [], 'groups': []}, '")
+split1 = name_list.split(
+    "', 'selected': False, 'topseller_groups': [], 'topseller_subcategories': [], 'groups': []}, '")
 # print(split1)
 index = 0
 
@@ -54,43 +56,48 @@ ind = 1
 # supplier_table2 = PrettyTable(['SN', 'Brand'])
 
 # while index_brand < len(split_brand)-1:
-    # split_brand2 = split_brand[index_brand+1].split("', 'selected'")
-    # index_brand = index_brand + 1
-    #supplier_table2.add_row([str(ind), split_brand2[0]])
-    # ind = ind + 1
+# split_brand2 = split_brand[index_brand+1].split("', 'selected'")
+# index_brand = index_brand + 1
+# supplier_table2.add_row([str(ind), split_brand2[0]])
+# ind = ind + 1
 
-supplier_table = PrettyTable(['SN', index_brand, 'Series', 'Link'])
+supplier_table = PrettyTable(['SN', 'Brand', 'Series', 'Series Link'])
 # print(len(split1))
-while index < len(split1) or index_brand < len(split_brand)-1:
-    spp = split1[index]
-    #split_brand2 = split_brand[index_brand + 1].split("', 'selected'")
-    # print(split1)
-    # break
 
-    if index == 0:
-        split2 = spp.split("{'")[1].split(",")[0]
-        # print(split2)
+with open('brand_data.csv', mode='w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    while index < len(split1) or index_brand < len(split_brand) - 1:
+        spp = split1[index]
+        # split_brand2 = split_brand[index_brand + 1].split("', 'selected'")
+        # print(split1)
         # break
-        split_brand2 = split_brand[index_brand + 1].split("', 'selected'")
-        link_generate = "https://www.druckerzubehoer.de/shop/subcategory/vcatid/DV-TinteToner/catid/" + split2 + "/lng/de_DE/site/1/lng/de_DE"
-        # print(link_generate)
-        supplier_table.add_row([str(index+1), split_brand2[0], split2, link_generate])
-    else:
-        split2 = spp.split("': {'name': '")[0].split(",")[0]
-        split_brand2 = split_brand[index_brand + 1].split("', 'selected'")
-        link_generate = "https://www.druckerzubehoer.de/shop/subcategory/vcatid/DV-TinteToner/catid/" + split2 + "/lng/de_DE/site/1/lng/de_DE"
-        supplier_table.add_row([str(index+1), split_brand2[0], split2, link_generate])
 
-    # link_generate = "https://www.druckerzubehoer.de/shop/subcategory/vcatid/DV-TinteToner/catid/"+split2+"/lng/de_DE/site/1/lng/de_DE"
-    index_brand = index_brand + 1
+        if index == 0:
+            split2 = spp.split("{'")[1].split(",")[0]
+            # print(split2)
+            # break
+            split_brand2 = split_brand[index_brand + 1].split("', 'selected'")
+            link_generate = "https://www.druckerzubehoer.de/shop/subcategory/vcatid/DV-TinteToner/catid/" + split2 + "/lng/de_DE/site/1/lng/de_DE"
+            # print(link_generate)
+            supplier_table.add_row([str(index + 1), split_brand2[0], split2, link_generate])
+            csv_writer.writerow([str(index + 1), split_brand2[0], split2, link_generate, ])
+        else:
+            split2 = spp.split("': {'name': '")[0].split(",")[0]
+            split_brand2 = split_brand[index_brand + 1].split("', 'selected'")
+            link_generate = "https://www.druckerzubehoer.de/shop/subcategory/vcatid/DV-TinteToner/catid/" + split2 + "/lng/de_DE/site/1/lng/de_DE"
+            supplier_table.add_row([str(index + 1), split_brand2[0], split2, link_generate])
+            csv_writer.writerow([str(index+1), split_brand2[0], split2, link_generate, ])
+        # link_generate = "https://www.druckerzubehoer.de/shop/subcategory/vcatid/DV-TinteToner/catid/"+split2+"/lng/de_DE/site/1/lng/de_DE"
+        index_brand = index_brand + 1
 
-    index = index + 1
+        index = index + 1
 
 print(supplier_table)
 
 stop = time.time()
 
-print('Took %.2f seconds to execute.' %(stop - start))
+print('Took %.2f seconds to execute.' % (stop - start))
 
 # index = 0
 # ind = 1
